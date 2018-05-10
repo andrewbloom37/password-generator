753772bf511cb378b553077252bda94ae415476c
Andrew Bloom <blooma116@gmail.com>
2018-05-10 07:39:38 -0700

    made script purely functional

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -9,10 +9,6 @@ const randomWord = arr => arr[Math.round(cryptoRando() * arr.length)];
 const randomNumber = () => String(Math.floor(cryptoRando() * 10));
 const coinFlip = () => Math.round(cryptoRando());
 
-const wordArr = [''];
+const createPassword = strArr => (strArr.join('').length < 24 ? createPassword([...strArr, (coinFlip() ? randomWord(words) : randomNumber())]) : strArr);
 
-while (wordArr.join('').length < 24) {
-  wordArr.push(coinFlip() ? randomWord(words) : randomNumber());
-}
-
-console.log(wordArr.map(e => e.split('').map((v, i) => i === 0 ? v.toUpperCase() : v).join('')).join(''));
+console.log(createPassword(['']).map(e => e.split('').map((v, i) => i === 0 ? v.toUpperCase() : v).join('')).join(''));

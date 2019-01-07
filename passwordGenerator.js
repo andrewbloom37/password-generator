fa3cfedc703f28c418958052733ecd0c06aa9b93
Andrew Bloom <blooma116@gmail.com>
2019-01-06 18:08:05 -0800

    updating script variables for readability, removing random numbers in password, shortening words to create more manageable passwords

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,15 +1,28 @@
+#!/usr/bin/env node
 const crypto = require('crypto');
 
 const words = Object.keys(JSON.parse(require('./words_dictionary')));
 
-const max32UInt = 4294967296;
-const cryptoRando = () => crypto.randomBytes(256).readUInt32LE() / max32UInt;
+const MAX_32_U_INT = 4294967296;
+const cryptographicallyRandomNumber = () =>
+  crypto.randomBytes(256).readUInt32LE() / MAX_32_U_INT;
 
-const randomWord = arr => arr[Math.round(cryptoRando() * arr.length)];
-const randomNumber = () => String(Math.floor(cryptoRando() * 10));
-const coinFlip = () => Math.round(cryptoRando());
+const validPosition = arr =>
+  Math.round(cryptographicallyRandomNumber() * arr.length);
 
-const createPassword = strArr => (strArr.join('').length < 24 ? createPassword([...strArr, (coinFlip() ? randomWord(words) : randomNumber())]) : strArr);
+const randomWord = arr => arr[validPosition(arr)];
+
+const toProperCase = word =>
+  word.split('').map((v, i) => i === 0 ? v.toUpperCase() : v).join('');
+
+const createPasswordArr = strArr =>
+  (strArr.join('').length < 24
+    ? createPasswordArr([...strArr, randomWord(words)])
+    : strArr);
+
+const createPassword = () =>
+  createPasswordArr(['']).map(toProperCase).join('');
 
 // only side-effect:
-console.log(createPassword(['']).map(e => e.split('').map((v, i) => i === 0 ? v.toUpperCase() : v).join('')).join(''));
+console.log(createPassword());
+

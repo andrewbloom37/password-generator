90dc3a05c6a6e292766588673fdeaebe509b426a
Andrew Bloom <blooma116@gmail.com>
2021-06-19 21:02:30 -0700

    reformat words to be array instead of object

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,23 +1,23 @@
 #!/usr/bin/env node
 const crypto = require('crypto');
 
-const words = Object.keys(require('./words_dictionary.json'));
+const words = require('./words.json');
 
 const MAX_32_U_INT = 4294967296;
-const cryptographicallyRandomNumber = () =>
+const getCryptographicallyRandomNumber = () =>
   crypto.randomBytes(256).readUInt32LE() / MAX_32_U_INT;
 
-const validPosition = arr =>
-  Math.round(cryptographicallyRandomNumber() * arr.length);
+const getValidPosition = () =>
+  Math.floor(getCryptographicallyRandomNumber() * words.length);
 
-const randomWord = arr => arr[validPosition(arr)];
+const getRandomWord = () => words[getValidPosition()];
 
 const toProperCase = word =>
   word.split('').map((v, i) => i === 0 ? v.toUpperCase() : v).join('');
 
 const createPasswordArr = (strArr = []) =>
-  (strArr.join('').length < 12 || strArr.length < 4
-    ? createPasswordArr([...strArr, randomWord(words)])
+  (strArr.length < 4 || strArr.join('').length < 12
+    ? createPasswordArr([...strArr, getRandomWord()])
     : strArr);
 
 const createPassword = () =>
